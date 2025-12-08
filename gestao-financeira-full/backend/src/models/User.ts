255957d398360f268cb3df3d7d27560520b1abca
markimrodrigues010-blip <markimrodrigues010@icloud.com>
2025-12-08 16:29:47 -0300

    Create User.ts

---
+++ gestao-financeira-full/backend/src/models/User.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password?: string;
+  createdAt: Date;
+}
+
+const UserSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<IUser>('User', UserSchema);
